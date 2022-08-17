771ddda79bb99185c9803d7594aef1c1be4bf0e3
Rodrigo Junior <rodrigojunior0323@gmail.com>
2022-08-17 11:55:08 -0300

    Continuação do desenvolvimento da landpage

--- src/pages/Landpage/Navbar/index.js
+++ src/pages/Landpage/Navbar/index.js
@@ -9,49 +9,70 @@ function Navbar() {
 
     const [navbarIsOpen, setNavbarIsOpen] = useState(false);
 
+    // quando tá aberto o menu e volta a tela:
+    //              - não reseta a visibilidade do menu
+    //              - o fundo não sai
+    //              - tirar o mobile navbar do menu
+    //              - ativar o menu fixado quando abrir ele direto (chamar o método de scroll)
+    //              - OK mobile navbar não sai quando redimensiona a tela
+    
     // fix the menu on the top of the page
     const scrollFunction = () => {
         if (document.body.scrollTop > 119 || document.documentElement.scrollTop > 119) {
             document.querySelector('.header').classList.add('fixed');
-            if (window.innerWidth <= 1299) {
-                console.log('teste');
-                document.querySelector('.header-nav').classList.add('mobile-navar');
-            }
         } else {
             document.querySelector('.header').classList.remove('fixed');
-            if (window.innerWidth > 1299) {
-                console.log('teste');
-                document.querySelector('.header-nav').classList.remove('mobile-navar');
-            }
         }
     }
 
-    window.onscroll = function() { scrollFunction() };
+    window.onscroll = function() { scrollFunction() }
 
     const handleNavbarisOpen = () => {
-
-        const nav = document.querySelector('nav');
-        const navLinks = document.querySelector('.navLinks');
-        const navActions = document.querySelector('.navActions');
-        const navBackground = document.querySelector('.nav-background');
-        
-        if (!navbarIsOpen) {
-            navLinks.classList.add('flex');
-            navActions.classList.add('flex');
-
-            nav.style.visibility = 'visible';
-            navBackground.style.visibility = 'visible';
-
-            setNavbarIsOpen(true);
-        } else {
-            navLinks.classList.remove('flex');
-            navActions.classList.remove('flex');
-
-            nav.style.visibility = 'hidden';
-            navBackground.style.visibility = 'hidden';
-
-            setNavbarIsOpen(false);
+        if (window.innerWidth > 1299) {
+            openNavbar(); /* TODO: ABRIR E FECHAR O MENU QUANDO REDIMENSIONA A TELA E PASSA DE 1299 */
+        } else if (window.innerWidth <= 1299) {
+            closeNavbar();
         }
+
+        if (!navbarIsOpen) {
+            openNavbar();
+        } else {
+            closeNavbar();
+        }
+    }
+
+    const openNavbar = () => {
+        const nav = document.querySelector('.header-nav');
+        const navLinks = document.querySelector('.nav-links');
+        const navActions = document.querySelector('.nav-actions');
+        const navBackground = document.querySelector('.nav-background');
+
+        nav.classList.add('mobile-navbar');
+
+        navLinks.classList.add('flex');
+        navActions.classList.add('flex');
+
+        nav.style.visibility = 'visible';
+        navBackground.style.visibility = 'visible';
+
+        setNavbarIsOpen(true);
+    }
+
+    const closeNavbar = () => {
+        const nav = document.querySelector('.header-nav');
+        const navLinks = document.querySelector('.nav-links');
+        const navActions = document.querySelector('.nav-actions');
+        const navBackground = document.querySelector('.nav-background');
+
+        nav.classList.remove('mobile-navbar');
+
+        navLinks.classList.remove('flex');
+        navActions.classList.remove('flex');
+
+        nav.style.visibility = 'hidden';
+        navBackground.style.visibility = 'hidden';
+
+        setNavbarIsOpen(false);
     }
 
     return (
@@ -64,19 +85,19 @@ function Navbar() {
                     <FontAwesomeIcon icon={faBars} />
                 </span>
                 <nav className="header-nav flex">
-                    <span className="navLinks">
+                    <span className="nav-links">
                         <a href="#hero">Início</a>
                         <a href="#explore">Explorar</a>
                         <a href="#subscriptions">Planos</a>
                         <a href="#about-us">Sobre</a>
                     </span>
-                    <span className="navActions">
+                    <span className="nav-actions">
                         <a href="/login">Conectar-se</a>
                         <a href="/cadastro" className="linkButton">Cadastrar-se</a>
                     </span>
                 </nav>
             </header>
-            <div class="nav-background"></div>
+            <div className="nav-background"></div>
         </Fragment>
     );
 }
